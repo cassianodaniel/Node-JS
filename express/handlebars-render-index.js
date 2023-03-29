8f2eab660f60dd09b2a5de23c1463560c4c741ad
Daniel Chaves <daniel.cassiano@live.com>
2023-03-29 12:37:14 -0300

    Update handlebars-render-index.js

--- express/handlebars-render-index.js
+++ express/handlebars-render-index.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const handlebars = require('express-handlebars');
+
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+
+app.set('view engine', 'handlebars');
 
 app.get('/cad', function(req,res){
    res.render('formulario')
